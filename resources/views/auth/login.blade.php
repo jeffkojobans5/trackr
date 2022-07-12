5060b1b362599457f5f7cfbed9d369bdbefac5a6
Jeff Bans <iambansah@gmail.com>
2022-07-12 22:03:54 +0000

    s

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -72,10 +72,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css')
     @vite('resources/css/custom.css')
     @vite('resources/js/app.js')
-    @vite('resources/js/custom.js')
+    @vite('resources/js/custom.js') --}}
 </html>
 
 
