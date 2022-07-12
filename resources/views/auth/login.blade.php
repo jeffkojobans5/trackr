a76da3634056e50202571a27b12a8a91df3b5fa7
Jeff Bans <iambansah@gmail.com>
2022-07-12 22:13:27 +0000

    m

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,6 +9,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script> 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>    
         <title>Trackr - Npontu</title>
+        
     </head>
 
 <body>
@@ -72,10 +73,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
-    {{-- @vite('resources/css/app.css')
+
+    @vite('resources/css/app.css')
     @vite('resources/css/custom.css')
     @vite('resources/js/app.js')
-    @vite('resources/js/custom.js') --}}
+    @vite('resources/js/custom.js')
 </html>
 
 
