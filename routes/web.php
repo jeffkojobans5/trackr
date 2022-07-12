f64df75003b4cc884ea7f3bafd93fb8c1ca4473d
Jeff Bans <iambansah@gmail.com>
2022-07-12 21:51:14 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ActivitiesController;
 use App\Http\Controllers\HistoryController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::group(['middleware' => ['auth']], function () { 
 Route::post('activity/create' , [ActivitiesController::class , 'store'])->name('createActivity');
